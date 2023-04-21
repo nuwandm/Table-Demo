a64e5f700c52e5c51d33bec10ff92efc6dd063b4
Nuwan Madusanka <nuwandarshana2012@gmail.com>
2023-04-21 11:44:11 +0530

    Created table instance

---
+++ src/component/BasicTable.js
@@ -0,0 +1,17 @@
+import { React, useMemo } from "react";
+import { UseTable, useTable } from "react-table";
+import MOCK_DATA from "./MOCK_DATA.json";
+import { COLUMNS } from "./columns";
+const BasicTable = () => {
+  // needs to memorize the rows and columns, data isn't recreated when an every render
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+  const tableInstance = useTable({
+    columns: columns,
+    data: data,
+    // this can be simplified because of ES6 shorthand syntax -->({columns,data})
+  });
+  return <div></div>;
+};
+
+export default BasicTable;
